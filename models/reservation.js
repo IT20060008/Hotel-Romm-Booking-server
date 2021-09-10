c8df5ff0152ea69611d6f90f6759429fb1a8c24f
Neelaka Rupun <rneelaka@gmail.com>
2021-09-11 01:18:26 +0530

    db data bind and server set

--- models/reservation.js
+++ models/reservation.js
@@ -1,31 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const reservationSchema = new Schema ({
-    
-    Customer_name : {
-        type : String,
-        required: true
+const reservationSchema = new Schema(
+  {
+    Customer_name: {
+      type: String,
+      required: true,
     },
 
-    National_Identy_Card_No:{
-        type : String,
-        required: true
+    National_Identy_Card_No: {
+      type: String,
+      required: true,
     },
 
-    Mobile:{
-        type : Number,
-        required: true
+    Mobile: {
+      type: Number,
+      required: true,
     },
 
-    Date:{
-        type : String,
-        required: true
-    }
-})
+    Email: {
+      type: String,
+      required: true,
+    },
 
+    From_Date: {
+      type: Date,
+      required: true,
+    },
 
-const reservation = mongoose.model("reservation",reservationSchema) ;
+    To_Date: {
+      type: Date,
+      required: true,
+    },
+
+    Payment_Option: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    collection: "reservation",
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
+
+const reservation = mongoose.model("reservation", reservationSchema);
 
 module.exports = reservation;
